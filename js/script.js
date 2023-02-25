f63b0a3fa5a41c9c7e0dd44b0e6facd9c8d8ab91
thedushyantsingh <119060348+thedushyantsingh@users.noreply.github.com>
2023-02-25 21:18:52 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -80,19 +80,19 @@ contactForm.addEventListener('submit', function (event) {
 
 // buton to top 
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("back-to-top-btn").style.display = "block";
-  } else {
-    document.getElementById("back-to-top-btn").style.display = "none";
-  }
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("back-to-top-btn").style.display = "block";
+    } else {
+        document.getElementById("back-to-top-btn").style.display = "none";
+    }
 }
 
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
 document.getElementById("back-to-top-btn").addEventListener("click", topFunction);
